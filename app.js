37d2ba63d45629634d2e15192e541056f33e081b
Mike Koboldt <mike.koboldt@starkbros.com>
2018-06-29 13:58:13 -0500

    finished the coursea assignment 1 for angular

--- app.js
+++ app.js
@@ -9,19 +9,26 @@
 	function lCheckerController($scope) {
 		$scope.input = "";
 		$scope.message = "";
+		$scope.color = "#ccc";
 
 		$scope.displayOutput = function () {
 			$scope.message = determineMessage($scope.input.split(',').map(item => item.trim()).filter(input => input.trim().length > 0), $scope);
 		}
+
+		$scope.clearInput = function () {
+			$scope.message = "";
+			$scope.input = "";
+			$scope.color = "#ccc";
+		}
 	}
 
 	function determineMessage(stringArr, $scope) {
-		if(stringArr.length < 0 ) {
-			$scope.color = "red";
+		if(stringArr.length <= 0 ) {
+			$scope.color = "#f00";
 			return "Please Enter valid Data!";
 		}
 		else {
-			$scope.color = "green";
+			$scope.color = "#008000";
 			return stringArr.length > 3 ? "Too Much!" : "Enjoy";
 		}
 	}
